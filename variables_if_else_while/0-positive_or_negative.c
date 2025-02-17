de074b70a569a64f27942f7719ec691d59dfc150
Your Name <you@example.com>
2025-02-17 15:04:47 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#includ <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -11,15 +11,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	{
 	if (n > 0);
-	("%d is positive,\n",n)
+	printf("%d is positive,\n",n)
 	}
 	{
 	else if (n = 0)
-	("%d is zero,\n",n)
+	printf("%d is zero,\n",n)
 	}
 	{
 	else (n < 0)
-	("%d is negative,\n"n)
+	print("%d is negative,\n"n)
 	}
 	return (0);
 }
