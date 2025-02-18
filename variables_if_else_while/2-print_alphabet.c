b6f364cf839e1a6b477bad4c1185e6eed430a28d
Your Name <you@example.com>
2025-02-18 14:01:49 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main(void)
-/*main:description of th project
+/**main:description of th project
  * blablabla */
 
 {
 	{
 	char ch;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-/* loop a a z */
+for (ch = 'a'; ch <= 'z'; ch++) /* loop a a z */
 	putchar(ch);
 	}
-	putchar('\n');
-	/*retour ligne*/
+	putchar('\n'); /*retour ligne*/
+
 	return (0);
 }
