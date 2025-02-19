596ebf9050807c90e7eeee3cb7efd9c3a4e8d10c
Your Name <you@example.com>
2025-02-19 13:43:25 +0000

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,20 +10,18 @@
  */
 int main(void)
 {
-    int ch;
+	int ch;
 
-    for (ch = '0'; ch <= '9'; ch++) /* Boucle de '0' à '9' */
-    {
-        putchar(ch); /* Affiche le chiffre */
-
-        if (ch != '9') /* Si ce n'est pas le dernier chiffre, ajoute ", " */
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n'); /* Nouvelle ligne */
-    return (0);
+	for (ch = '0'; ch <= '9'; ch++) /* Boucle de '0' à '9' */
+	{
+		putchar(ch); /* Affiche le chiffre */
+		if (ch != '9') /* Si ce n'est pas le dernier chiffre, ajoute ", " */
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n'); /* Nouvelle ligne */
+	return (0);
 }
 
