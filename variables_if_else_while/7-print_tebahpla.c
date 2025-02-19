50d469ce65d2c33fa3db85129dd5d20c44617e4a
Your Name <you@example.com>
2025-02-19 10:24:38 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Description: This program uses a for loop to print
+ * all lowercase letters from 'a' to 'z', followed by
+ * a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++) /** loop a a z */
+	{
+		putchar(ch);
+	}
+	{
+	putchar('\n'); /**geeghhgerejr*/
+	}
+	return (0);
+}
