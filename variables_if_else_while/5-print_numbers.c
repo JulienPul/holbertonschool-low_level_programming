107633a09f0c2a4fd0ee145f3d4c20aed7e87b42
Your Name <you@example.com>
2025-02-19 09:29:34 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h> /* Missing header for printf */
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random number and determines if it's
+ * positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	{
+		putchar('\n');
+	}
+	return (0);
+}
