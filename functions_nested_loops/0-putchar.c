cfba57e2e972aeddaa75beacba974de5314dcfeb
Your Name <you@example.com>
2025-02-19 14:42:19 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar
+ *
+ * Description: The program should return 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+	{
+for (ch = '_'; ch = 'p'; ch = 'u'; ch = 't'; ch = 'c'; ch = 'h'; ch = 'a'; ch = 'r';)
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
