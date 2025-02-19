39eb374752674bac937c97f7fa450197387b178f
Your Name <you@example.com>
2025-02-19 08:39:46 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Description: This program uses a for loop to print
+ * all lowercase letters from 'a' to 'z', followed by
+ * a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++) /** loop a a z */
+	{
+		putchar(ch);
+	}
+	if (ch !'q' && ch !'e'); /**exclusion q et e */
+	{
+1	putchar('\n'); /**geeghhgerejr*/
+	}
+
+	return (0);
+}
