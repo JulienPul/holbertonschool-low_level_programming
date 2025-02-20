50b298582532f653396782c2b0cf6a5f94d8bef1
Your Name <you@example.com>
2025-02-20 14:30:57 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-/**
- * main - Prints _putchar
+/**dgdgeggejgfdhegfejdej
  *
- * Description: The program should return 0
- *
- * Return: Always 0 (Success)
- */
+ * bdbjdebdj*/
 int main(void)
 {
-	{
-putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	{	putchar(ch)
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
 	}
 	putchar('\n');
 	return (0);
-}
+	
