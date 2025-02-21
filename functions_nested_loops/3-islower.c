f1f777f89d2e43e3d2b6b3ed8d660700bb6694c1
Your Name <you@example.com>
2025-02-21 12:09:24 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int is_lower - Prints the alphabet in lowercase x10
+ * Description : This program uses a for loop to print
+ * all lowercase letters from 'a' to 'z', followed by
+ * a new line.
+ * Return : Always 0 (Success)
+ */
+int _islower(int c);
+char ch;
+{
+	{
+	if (islower(ch)) 
+	
+		_putchar("%c 1\n", ch);
+	}
+	{
+	else {
+		_putchar("%c 0\n", ch);
+	}
+
+    return 0;
+}
