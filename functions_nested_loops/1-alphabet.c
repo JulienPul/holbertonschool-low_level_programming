f43ade017b09cfc71ca09e196d40668fcbcfe108
Your Name <you@example.com>
2025-02-21 08:24:43 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Description: This program uses a for loop to print
+ * all lowercase letters from 'a' to 'z', followed by
+ * a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char (ch);
+	
+	for (ch = 'a'; ch <= 'z'; ch++) /** loop a a z */
+	{
+		_putchar(ch);
+	}
+	_putchar('\n'); /**geeghhgerejr*/
+
+	return (0);
+}
