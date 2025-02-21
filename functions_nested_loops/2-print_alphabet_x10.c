eb834285a9ebddbe6548ed0a148aa9844de157ec
Your Name <you@example.com>
2025-02-21 10:19:36 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet - Prints the alphabet in lowercase x10
+ *
+ * Description: This program uses a for loop to print
+ * all lowercase letters from 'a' to 'z', followed by
+ * a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char (ch);
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (ch = 'a'; ch <= 'z'; ch++) /** loop a a z */
+		{
+		_putchar(ch);
+		}
+	}
+	_putchar('\n'); /**geeghhgerejr*/
+}
