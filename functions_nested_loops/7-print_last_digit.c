be36505eb2c6feb6815a51384577d89faee58400
Your Name <you@example.com>
2025-02-23 13:19:20 +0000

    7-last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,12 @@
  * @n: number of the last digit
  *
  * Return: last digit
- * */
-int print_last_digit(int n);
+ */
+int print_last_digit(int n)
 {
 	int last_digit;
 
-	last digit = n % 10;
+	last_digit = n % 10;
 
 	if (last_digit < 0)
 		last_digit = -last_digit;
