c23cdc8710838cc55a5f7489de99f4a557fdc775
Your Name <you@example.com>
2025-02-23 14:29:27 +0000

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - adds two integers
+* * Description: a+b and result
+* * @a: integer a
+* @b: integer b
+* Return: the result
+*/
+int add(int a, int b)
+	{
+		return (a + b);
+	}
