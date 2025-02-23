178e1189f94eb965bb0cd370fa57637d9e6c9c98
Your Name <you@example.com>
2025-02-23 14:09:11 +0000

    8-24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /*
- * 24_hours: prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * 24_hours: prints every minute of the day, start 00:00 to 23:59.
  * Description: jack bauer
  *
  * Return: (0)
  */
- void jack_bauer(void)
+void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0;hour < 24; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
