9ac03bcb4f0f9773ab8943411ba8ec79faab6f56
Your Name <you@example.com>
2025-02-24 10:35:22 +0000

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - 2 integers
+ * Description: bnjbdujdhf
+ * @a:first integer
+ * @b: second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
