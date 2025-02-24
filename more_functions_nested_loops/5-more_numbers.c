ee7c25a7c1b25ff3886b035fcaa70a1e8d4ca880
Your Name <you@example.com>
2025-02-24 14:10:16 +0000

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - dkojfjf
+ * Description: jhjghgf
+ * @n: ,rkfjkrkh
+ * @i: dklkjfhguf
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar((n / 10) + '0');
+
+			_putchar((n % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
