3fdad9ce9c62d0be3128f0ba42b35a02d7ec5694
Your Name <you@example.com>
2025-02-24 09:45:06 +0000

    0-isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@
 /**
  * _isupper - check the code.
  *Description: fhfhfyryrggr
- @c: character
+ *@c: character
  * Return: Always 0.
  */
 int _isupper(int c)
-	
+
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
