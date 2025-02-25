ed0e6bc6bc6c1ed65cc4c595c106709c1d901a0a
Your Name <you@example.com>
2025-02-25 09:49:13 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,6 @@
  * print_diagonal- draw diagonal line in the terminal
  * Description: You can only use _putchar function to print
  *@n: kdojdl
- * Return: 0
  */
 void print_diagonal(int n)
 {
@@ -13,17 +12,19 @@ int i;
 		{
 			_putchar('\n');
 		}
+		else
 	{
 		for (i = 0; i < n; i++)
 		{
 			int j;
-			
-			for (j = 0; j < n; j++)
+
+			for (j = 0; j < i; j++)
 
 			_putchar(' ');
-		}
+
 			_putchar('\\');
-	
+
 			_putchar('\n');
+		}
 	}
 }
