099e0bb23f968da9facb1d569760bf2437430ef1
Your Name <you@example.com>
2025-02-26 10:07:46 +0000

    1-swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * swap -  swaps the values of two integers.
+ * swap_int -  swaps the values of two integers.
  * Description: jijhfg
- *t: variable temporire
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  */
 void swap_int(int *a, int *b)
 {
