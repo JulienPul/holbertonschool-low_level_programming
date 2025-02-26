c20b9a0bb10d0f4b9a7138b99560e7c902c644d7
Your Name <you@example.com>
2025-02-26 09:34:15 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * description: and updates the value it points to to 98.
+ * @n: lkoj
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
