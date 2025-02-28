6de172200bd49dbd504024c049cdd5eb8d04bf9c
Your Name <you@example.com>
2025-02-28 10:17:27 +0000

    5-rev_string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
- * print_rev -  prints a string, in reverse, followed by a new line
- * @s: kjdjhee
+ * print_rev - Affiche une chaîne de caractères en ordre inverse.
+ * @s: Pointeur vers la chaîne de caractères.
  */
 void print_rev(char *s)
 {
-	int len = 0;
-	int i;
+    int len;
 
-		for (len = 0; s[len] != '\0'; len++)
-		{
-			for (i = len - 1; i >= 0; i--)
-			{
-				_putchar(s[i]);
-			}
-				_putchar('\n');
-			}
+    for (len = 0; s[len] != '\0'; len++)
+        ;
+    for (len = len - 1; len >= 0; len--)
+    {
+        _putchar(s[len]);
+    }
+    _putchar('\n');
 }
+
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string -  reverses a string.
+ * @s: pointers to the string
+ */
+void rev_string(char *s)
+{
+	int i ;
+	int j;
+	int len = 0;
+	char temp;
+	
+	for (len = 0; s[len] != '\0'; len++);
+	for (i = 0, j = len -1; i < j; i++,j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}		
