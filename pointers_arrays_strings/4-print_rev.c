316fcb1b69faed471a2f92a01eb04231898a34e0
Your Name <you@example.com>
2025-03-01 14:42:55 +0000

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,13 @@
  */
 void print_rev(char *s)
 {
-    int len;
+	int len;
 
-    for (len = 0; s[len] != '\0'; len++)
+	for (len = 0; s[len] != '\0'; len++)
         ;
-    for (len = len - 1; len >= 0; len--)
-    {
-        _putchar(s[len]);
-    }
-    _putchar('\n');
+	for (len = len - 1; len >= 0; len--)
+	{
+	_putchar(s[len]);
+	}
+	_putchar('\n');
 }
-
