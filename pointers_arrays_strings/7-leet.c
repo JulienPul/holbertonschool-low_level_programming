6c5dab6918b98992ed04084f5b0a7bbe203c847d
Your Name <you@example.com>
2025-03-02 08:11:06 +0000

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,19 +9,19 @@
 char *leet(char *str)
 {
 	int i, j;
-	
+
 	char letters[] = "aAeEoOtTlL";
 	char leet[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	for (j = 0; letters[j] != '\0'; j++)
-	{
-		if (str[i] == letters[j])
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-	str[i] = leet[j];
+			if (str[i] == letters[j])
+			{
+				str[i] = leet[j];
+			}
 		}
-	}
 	}
 
 	return (str);
