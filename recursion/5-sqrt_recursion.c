dccf9165f046fab1a7d427ba54c4b3e474a2febf
Your Name <you@example.com>
2025-03-07 14:47:25 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,9 +10,9 @@
 int helper_sqrt(int n, int i)
 {
 	if (i * i == n)
-	return i;
+	return (i);
 	if (i * i > n)
-	return -1;
+	return (-1);
 	return helper_sqrt(n, i + 1);
 }
 
@@ -25,6 +25,6 @@ int helper_sqrt(int n, int i)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	return -1;
+	return (-1);
 	return helper_sqrt(n, 0);
 }
