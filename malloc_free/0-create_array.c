51b3236d42d1e6d22619af9702c18caaa9b5b46f
Your Name <you@example.com>
2025-03-10 11:38:22 +0000

    1-strdup.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates an array of chars, initializes it with specific char.
+ * @c: character to initialise array
+ * @size: size of array
+ * Return: pointer to array, or null if fail or size = 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+
+	if (size == 0)
+		return (NULL);
+
+	arr = (char *)malloc(size * sizeof(char));
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	arr[i] = c;
+	return (arr);
+}
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup - ffehehfeh
+ * @str: string
+ * Return: Null str=Null ,sucess *str, Null if error
+ */
+char *_strdup(char *str)
+{
+	int len, i;
+	char *dup;
+
+	if (str == 0)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+
+	dup = (char *) malloc((len + 1) * sizeof(char)); /*inclure nulbyte*/
+		if (dup == 0)
+			return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+
+			return (dup);
+}
