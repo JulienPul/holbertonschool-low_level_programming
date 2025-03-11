53d625efef90d55dabfd2203d66a86a23b6c37c6
Your Name <you@example.com>
2025-03-11 14:17:20 +0000

    more_malloc_free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: number of bytes allocated
+ * Return: 98 if fails, return pointer to alloc memory
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(b);
+if (ptr == NULL)
+	exit (98);
+return (ptr);
+}	
