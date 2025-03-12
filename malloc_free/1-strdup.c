8180f7dcc38da78c6fb3f86759bfd3b6751e46f9
Your Name <you@example.com>
2025-03-12 14:43:10 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 
 	for (len = 0; str[len] != '\0'; len++)
 
-	dup = (char *) malloc((len + 1) * sizeof(char)); /*inclure nulbyte*/
+	dup = malloc((len + 1) * sizeof(char));
 		if (dup == 0)
 			return (NULL);
 
