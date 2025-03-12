930f9ae90877add82e4cd7418c0695527182006a
Your Name <you@example.com>
2025-03-12 16:06:38 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  struct dog - Structure représentant un chien
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (flottant)
+ * @owner: Propriétaire du chien (chaîne de caract�s)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
