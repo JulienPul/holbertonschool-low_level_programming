ce6ab04e19533cc9928cbdb47b25a023818e9f11
Your Name <you@example.com>
2025-03-14 11:51:08 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,6 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 /**
  * dog_t - new name for the type struct dog.
  * @name: Nom du chien (chaîne de caractères)
@@ -20,12 +22,9 @@ struct dog
  * @owner: Propriétaire du chie
  */
 
-typedef struct dog_t
+typedef struct dog
 {
 	char *name;
 	float age;
-	char *owner;}dog;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+	char *owner;}dog_t;
 #endif
