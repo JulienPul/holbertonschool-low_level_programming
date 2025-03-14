6cb31cf5f69a51d5853524ef7b8167e1f8641483
Your Name <you@example.com>
2025-03-14 13:23:47 +0000

    4-free_grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-free_grid -  frees a 2 dimensional grid created by your alloc_grid function.
-@grid: Pointer to the 2D array
-@height:Number of rows in the grid
+*free_grid -  frees a 2 dimensional grid created by your alloc_grid.
+*@grid: Pointer to the 2D array
+*@height:Number of rows in the grid
 */
 void free_grid(int **grid, int height)
 {
