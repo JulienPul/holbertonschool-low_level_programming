8d053991517caa54cd0bfcbd7e4c00cf6e4f29c1
Your Name <you@example.com>
2025-03-17 14:22:50 +0000

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,9 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL && array == NULL)
+	if (cmp == NULL || array == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
