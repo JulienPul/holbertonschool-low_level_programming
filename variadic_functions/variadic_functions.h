995c19c5cd71c60e6d10a9d0e3c50c74a8cc31bb
Pulon Julien <pulonjulien@gmail.com>
2025-03-19 14:53:18 +0100

    1-print_numbers.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
-#ifndef C1766AC0_694E_4A3F_ABC7_C5F89AA5F3DE
-#define C1766AC0_694E_4A3F_ABC7_C5F89AA5F3DE
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
