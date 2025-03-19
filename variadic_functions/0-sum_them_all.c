84422d3a520afe0afbbe33dd899304a130eb4fae
Pulon Julien <pulonjulien@gmail.com>
2025-03-19 11:06:35 +0100

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,22 +3,24 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - func return sum of all parameters
- * 
- * Return: if n=0 return 0, return sum*/
+ *@n: nb args
+ * Return: if n=0 return 0, return sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	int total = 0;
+	va_list args;
+	unsigned int i;
 
 	if (n == 0)
-	return(0);
+		return (0);
 
-	va_list args;
 	va_start(args, n);
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		total += va_arg(args, int);
 	}
 	va_end(args);
-	return(total);
+		return (total);
 }
