5eb9146c6062d60146fd006868a78fc458cab2fe
Pulon Julien <pulonjulien@gmail.com>
2025-03-31 14:48:26 +0200

    2-add_node.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef C9A32705_16F7_4B5F_AFF2_E9C1BD984D62
 #define C9A32705_16F7_4B5F_AFF2_E9C1BD984D62
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,5 +21,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* C9A32705_16F7_4B5F_AFF2_E9C1BD984D62 */
