c26ba773b7e1e1951523955b40557f9c0a0763a4
Pulon Julien <pulonjulien@gmail.com>
2025-03-31 12:58:58 +0200

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 /**
  * list_len - function that returns the number of elements in linked list_t
  * @h: head of the list
- * Return number of elements in linked list
+ * Return: number of elements in linked list
  */
 size_t list_len(const list_t *h)
 {
