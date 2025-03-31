fd4a358b953ceee6ae1b2b942187daf027954e4d
Pulon Julien <pulonjulien@gmail.com>
2025-03-31 15:19:42 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,6 +8,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+	if (str == NULL)
+		return (NULL);		
 new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
