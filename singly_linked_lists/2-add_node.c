3eb88fad220d35574ceee13518f9738986e05cb3
Pulon Julien <pulonjulien@gmail.com>
2025-04-01 10:14:49 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,23 +1,4 @@
 #include "lists.h"
-
-/**
- * _strlen - Returns the length of a string
- * @s: The string whose length is to be determined
- *
- * Return: Length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-	len++;
-
-	}
-	return (len);
-}
-
 /**
  * add_node -  function that adds a new node at the beginning of a list_t
  * @head: Pointer to the pointer of the head of the list
@@ -28,7 +9,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 	if (str == NULL)
-		return (NULL);		
+		return (NULL);
+
 new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
