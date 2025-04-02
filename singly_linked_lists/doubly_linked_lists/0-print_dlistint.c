5d507e4b6eab0e0adaa30bec10ff91ff374167e9
Pulon Julien <pulonjulien@gmail.com>
2025-04-02 10:33:06 +0200

    print_dlistint

--- singly_linked_lists/doubly_linked_lists/0-print_dlistint.c
+++ singly_linked_lists/doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *temp = h;
 
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
