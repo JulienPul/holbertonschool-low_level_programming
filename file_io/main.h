0cc4766af663a9d0735dc39f77335c71212381f8
Pulon Julien <pulonjulien@gmail.com>
2025-04-06 19:02:25 +0200

    create_file

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,10 @@
-#ifndef A2A33DA7_BCBA_4522_9183_E8F53CAF6FE6
-#define A2A33DA7_BCBA_4522_9183_E8F53CAF6FE6
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 
-#endif /* A2A33DA7_BCBA_4522_9183_E8F53CAF6FE6 */
+#endif /* MAIN_H */
