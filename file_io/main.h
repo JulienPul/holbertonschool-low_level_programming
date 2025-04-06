c75d0bea0a8d1b40cafbb11119761c2bb172ac08
Pulon Julien <pulonjulien@gmail.com>
2025-04-06 19:00:04 +0200

    create_file

--- file_io/main.h
+++ file_io/main.h
@@ -4,5 +4,6 @@
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* A2A33DA7_BCBA_4522_9183_E8F53CAF6FE6 */
